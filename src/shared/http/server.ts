d6fe6ed3716e0107ae1fb6278c26f628dcffc47e
rodrigusto <rodrigusto@gmail.com>
2021-07-06 14:37:08 -0300

    inclusão do Celebrate, para validação de dados

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
+import { errors } from 'celebrate';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 
@@ -13,6 +14,8 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use(errors());
+
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
@@ -21,6 +24,7 @@ app.use(
         message: error.message,
       });
     }
+
     return response.status(500).json({
       status: 'error',
       message: 'Internal server error',
