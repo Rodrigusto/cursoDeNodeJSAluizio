22653c83ec6c842f2dced1a74602553996a01d75
rodrigusto <rodrigusto@gmail.com>
2021-09-08 15:44:43 -0300

    Migrations Orders

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,13 +4,15 @@ import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
+import customersRouter from '@modules/customers/routes/customers.routes';
 
 const routes = Router();
 
-routes.use('/products', productsRouter);
-routes.use('/users', usersRouter);
-routes.use('/sessions', sessionsRouter);
+routes.use('/customers', customersRouter);
 routes.use('/password', passwordRouter);
+routes.use('/products', productsRouter);
 routes.use('/profile', profileRouter);
+routes.use('/sessions', sessionsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
