169c3a1e08e3cea509a1deb0cd94ab7a85eefc50
rodrigusto <rodrigusto@gmail.com>
2021-09-14 16:06:43 -0300

    orders crud

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -5,10 +5,12 @@ import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
 import customersRouter from '@modules/customers/routes/customers.routes';
+import ordersRouter from '@modules/orders/routes/orders.routes';
 
 const routes = Router();
 
 routes.use('/customers', customersRouter);
+routes.use('/orders', ordersRouter);
 routes.use('/password', passwordRouter);
 routes.use('/products', productsRouter);
 routes.use('/profile', profileRouter);
