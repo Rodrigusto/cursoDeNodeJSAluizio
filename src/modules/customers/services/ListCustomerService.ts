aa5a23d074adef7bc83b86a462aa55760dd197ea
rodrigusto <rodrigusto@gmail.com>
2021-09-20 15:32:52 -0300

    pagination by typeorm

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -2,14 +2,23 @@ import { getCustomRepository } from 'typeorm';
 import Customer from '../typeorm/entities/Customer';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 
-class ListCustomerService {
-  public async execute(): Promise<Customer[]> {
-    const customersRepository = getCustomRepository(CustomersRepository);
-
-    const customers = customersRepository.find();
-
-    return customers;
-  }
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
 }
 
-export default ListCustomerService;
+export default class ListCustomerService {
+  public async execute(): Promise<IPaginateCustomer> {
+    const customersRepository = getCustomRepository(CustomersRepository);
+
+    const customers = await customersRepository.createQueryBuilder().paginate();
+
+    return customers as IPaginateCustomer;
+  }
+}
